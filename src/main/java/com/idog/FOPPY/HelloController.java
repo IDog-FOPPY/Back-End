9abca0a2702c83d6b1d06a33ccc465453265882a
jxxxxharu <jxxxxharu@gmail.com>
2023-06-06 10:20:05 +0900

    Hello FOPPY!

---
+++ src/main/java/com/idog/FOPPY/HelloController.java
@@ -0,0 +1,14 @@
+package com.idog.FOPPY;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello FOPPY!";
+    }
+
+}
