7a9c302aa482501c264c63d8d324c8a5976022b3
jxxxxharu <jxxxxharu@gmail.com>
2023-06-09 18:53:46 +0900

    [Fix] SecurityConfig import error

--- src/main/java/com/idog/FOPPY/config/SecurityConfig.java
+++ src/main/java/com/idog/FOPPY/config/SecurityConfig.java
@@ -1,23 +1,17 @@
 package com.idog.FOPPY.config;
 
-//import com.idog.FOPPY.component.CustomAuthenticationFailureHandler;
-import com.idog.FOPPY.component.CustomAuthenticationSuccessHandler;
 import com.idog.FOPPY.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @Slf4j
