3049d46fd3eefe840d2b9a44a2298f19732bcbd4
jxxxxharu <jxxxxharu@gmail.com>
2023-06-18 20:37:06 +0900

    [Fix] 요청을 json 형식으로 받도록

--- src/main/java/com/idog/FOPPY/controller/MemberController.java
+++ src/main/java/com/idog/FOPPY/controller/MemberController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class MemberController {
 
     @Operation(summary = "회원가입")
     @PostMapping("/join")
-    public ResponseEntity<String> saveMember(MemberDTO memberDTO) {
+    public ResponseEntity<String> saveMember(@RequestBody MemberDTO memberDTO) {
 //        return memberService.saveMember(memberDTO);
         return ResponseEntity.ok(memberService.saveMember(memberDTO));
 //        memberService.saveMember(memberDTO);
@@ -29,7 +30,7 @@ public class MemberController {
 
     @Operation(summary = "로그인")
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(MemberDTO memberDTO) {
+    public ResponseEntity<LoginResponse> login(@RequestBody MemberDTO memberDTO) {
         LoginResponse response = memberService.login(memberDTO);
         return ResponseEntity.ok(response);
     }
