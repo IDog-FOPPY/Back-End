2a2557349b5244d29735fdaf0e6c82e3a6dcf5e8
jxxxxharu <jxxxxharu@gmail.com>
2023-07-19 23:18:36 +0900

    [Cherrypick,Feat,Fix] refresh token 발급, JWT 수정

--- src/main/java/com/idog/FOPPY/dto/User/LoginUserResponse.java
+++ src/main/java/com/idog/FOPPY/dto/User/LoginUserResponse.java
@@ -1,13 +1,20 @@
 package com.idog.FOPPY.dto.User;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class LoginUserResponse {
+
     private Long userId;
-    private String token;
+    private String email;
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
 }
---
+++ src/main/java/com/idog/FOPPY/service/JwtService.java
@@ -0,0 +1,27 @@
+package com.idog.FOPPY.service;
+
+import com.idog.FOPPY.config.jwt.JwtProvider;
+import com.idog.FOPPY.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken) {
+        if(!jwtProvider.validateToken(refreshToken)) {
+            throw new IllegalArgumentException("Invalid refresh token");
+        }
+
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getId();
+        User user = userService.findById(userId);
+
+//        return jwtProvider.createToken(member, Duration.ofHours(1));
+        return jwtProvider.createAccessToken(user, 24 * 60 * 60 * 1000L);
+    }
+}
