8fb8c9185097c6c2236ca707190b86b9f3dfc1a0
jxxxxharu <jxxxxharu@gmail.com>
2023-08-20 22:19:42 +0900

    ChatRoomDTO.Detail에 프로필 사진 URL 추가

--- src/main/java/com/idog/FOPPY/dto/chat/ChatRoomDTO.java
+++ src/main/java/com/idog/FOPPY/dto/chat/ChatRoomDTO.java
@@ -74,16 +74,32 @@ public class ChatRoomDTO {
         private Long member2Id;
         private String member1NickName;
         private String member2NickName;
+        private String member1ProfileImage;
+        private String member2ProfileImage;
         private String createdAt;
         private List<ChatMessageDTO.Response> chatMessages;
 
         public static Detail of(ChatRoom chatRoom) {
+            String member1ProfileImage, member2ProfileImage;
+            try {
+                member1ProfileImage = chatRoom.getMember1().getDogs().get(0).getImgUrlList().get(0);
+            } catch (Exception e) {
+                member1ProfileImage = "https://기본프사";
+            }
+            try {
+                member2ProfileImage = chatRoom.getMember2().getDogs().get(0).getImgUrlList().get(0);
+            } catch (Exception e) {
+                member2ProfileImage = "https://기본프사";
+            }
+
             return Detail.builder()
                     .id(chatRoom.getId())
                     .member1Id(chatRoom.getMember1().getId())
                     .member2Id(chatRoom.getMember2().getId())
                     .member1NickName(chatRoom.getMember1().getNickName())
                     .member2NickName(chatRoom.getMember2().getNickName())
+                    .member1ProfileImage(member1ProfileImage)
+                    .member2ProfileImage(member2ProfileImage)
                     .createdAt(chatRoom.getCreatedAt().toString())
                     .chatMessages(chatRoom.getChatMessages().stream().map(ChatMessageDTO.Response::of).toList())
                     .build();
