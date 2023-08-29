47afd2dc0ae9b47259fbac7ed01a28e89d82e628
ddrongy <ddrongy@gmail.com>
2023-08-30 01:13:03 +0900

    fix: timezone 설정

--- src/main/java/com/idog/FOPPY/FoppyApplication.java
+++ src/main/java/com/idog/FOPPY/FoppyApplication.java
@@ -1,14 +1,22 @@
 package com.idog.FOPPY;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @EnableWebMvc
 @SpringBootApplication
 public class FoppyApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoppyApplication.class, args);
